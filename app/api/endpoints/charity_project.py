dccb0c9824ba5b87733c090f5ff9de6ada421de6
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-03-13 19:55:20 +0300

    feat: create patch endpoint for charity projects, add validator for it

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.validators import check_has_investment, check_name_is_busy
+from app.api.validators import check_has_investment, check_name_is_busy, \
+    check_eligible_for_patching
 from app.core.db import get_async_session
-from app.crud.charity_project import charity_crud
-from app.schemas.charity_project import CharityProjectDB, CharityProjectCreate
 from app.core.user import current_superuser
+from app.crud.charity_project import charity_crud
+from app.schemas.charity_project import CharityProjectDB, CharityProjectCreate, \
+    CharityProjectUpdate
 
 router = APIRouter()
 
@@ -36,7 +38,8 @@ async def create_project(
 @router.delete(
     "/{project_id}",
     response_model=CharityProjectDB,
-    dependencies=[Depends(current_superuser)])
+    dependencies=[Depends(current_superuser)]
+)
 async def delete_project(
         project_id: int,
         session: AsyncSession = Depends(get_async_session)
@@ -46,7 +49,17 @@ async def delete_project(
     return await charity_crud.delete(project, session)
 
 
-@router.patch("/{project_id}")
-async def update_project():
+@router.patch(
+    "/{project_id}",
+    response_model=CharityProjectDB,
+    dependencies=[Depends(current_superuser)],
+    response_model_exclude_none=True)
+async def update_project(
+        project_id: int,
+        obj_in: CharityProjectUpdate,
+        session: AsyncSession = Depends(get_async_session)
+):
     """Superusers only."""  # FIXME ADD ACTUAL DOCSTRING AND DESCRIPTION
-    pass
+    project = await check_eligible_for_patching(project_id, obj_in, session)
+    project = await charity_crud.patch(project, obj_in, session)
+    return project
--- app/api/validators.py
+++ app/api/validators.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.charity_project import charity_crud
 from app.models import CharityProject
+from app.schemas.charity_project import CharityProjectUpdate
+from app.services.constants import ErrConstants as Err
 
 
 async def check_name_is_busy(
@@ -14,7 +16,7 @@ async def check_name_is_busy(
     project_id = await charity_crud.get_project_id_by_name(name, session)
     if project_id is not None:
         raise HTTPException(
-            HTTPStatus.BAD_REQUEST, "TO BE CHANGED LATER!"  # FIXME
+            HTTPStatus.BAD_REQUEST, Err.NAME_IS_BUSY
         )
 
 
@@ -22,8 +24,29 @@ async def check_has_investment(project_id: int,
                                session: AsyncSession) -> CharityProject:
     project: CharityProject = await charity_crud.get(project_id, session)
     if project is None:
-        raise HTTPException(HTTPStatus.BAD_REQUEST, "Not Found")
+        raise HTTPException(HTTPStatus.BAD_REQUEST, Err.NOT_FOUND)
     if project.invested_amount > 0:
         raise HTTPException(HTTPStatus.BAD_REQUEST,
-                            "TO BE CHANGED LATER!")  # FIXME
+                            Err.CANNOT_DELETE_PROJECT_WITH_INVESTMENTS)
+    return project
+
+
+async def check_eligible_for_patching(
+        project_id,
+        data_in: CharityProjectUpdate,
+        session: AsyncSession
+) -> CharityProject:
+    project: CharityProject = await charity_crud.get(project_id, session)
+    if project is None:
+        raise HTTPException(HTTPStatus.NOT_FOUND,
+                            Err.NOT_FOUND)
+    if project.fully_invested:
+        raise HTTPException(HTTPStatus.BAD_REQUEST,
+                            Err.CANNOT_MODIFY_CLOSED_PROJECT)
+    if (data_in.full_amount and
+            data_in.full_amount < project.invested_amount):
+        raise HTTPException(HTTPStatus.BAD_REQUEST,
+                            Err.FULL_AMOUNT_LT_INVESTED_AMOUNT)
+    if data_in.name != project.name:
+        await check_name_is_busy(data_in.name, session)
     return project
