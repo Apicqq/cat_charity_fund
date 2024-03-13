9bb11cdd8a2a2095bdd3874baa893c2bb7cfce1a
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-03-13 10:22:48 +0300

    feat: add superuser creation on the first startup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
-
+from core.init_db import create_first_superuser
 
 app = FastAPI(
     title=settings.app_title,
     description=settings.description
 )
+
+
+@app.on_event("startup")
+async def startup():
+    await create_first_superuser()
