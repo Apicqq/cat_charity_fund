ecdf456f92d408feb9da67f997f779e31ec48cca
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-03-13 10:19:11 +0300

    feat: move metadata param to the right place for it to take effect

--- app/core/db.py
+++ app/core/db.py
@@ -7,14 +7,6 @@ from app.core.config import settings
 
 class PreBase:
 
-    metadata = MetaData(naming_convention={
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s"
-    })
-
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
@@ -22,7 +14,13 @@ class PreBase:
     id = Column(Integer, primary_key=True)
 
 
-Base = declarative_base(cls=PreBase)
+Base = declarative_base(cls=PreBase, metadata=MetaData(naming_convention={
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}))
 engine = create_async_engine(settings.database_url)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
 
