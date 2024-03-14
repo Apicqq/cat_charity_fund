fd5ea3a571553a80f1ef9fc9281f5ed1a99f8c55
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-03-14 19:03:24 +0300

    chore: change import name

--- app/core/config.py
+++ app/core/config.py
@@ -2,14 +2,14 @@ from typing import Optional
 
 from pydantic import BaseSettings, EmailStr
 
-from app.services.constants import ConfigConstants as Cconst
+from app.services.constants import ConfigConstants as ConfigConst
 
 
 class Settings(BaseSettings):
-    app_title: str = Cconst.APP_TITLE
-    description: str = Cconst.DESCRIPTION
-    database_url: str = Cconst.DATABASE_URL
-    secret: str = Cconst.SECRET
+    app_title: str = ConfigConst.APP_TITLE
+    description: str = ConfigConst.DESCRIPTION
+    database_url: str = ConfigConst.DATABASE_URL
+    secret: str = ConfigConst.SECRET
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
 
